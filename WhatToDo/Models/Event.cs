a60c9a8a72c1c866478488f5c8b7ce330b35ed87
Всеволод Паршин <seva.parshin@yandex.ru>
2019-05-25 22:16:13 +0300

    feat: add Entity Framework

---
+++ WhatToDo/Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WhatToDo.Models
+{
+    public partial class Event
+    {
+        public Event()
+        {
+            PlaceEventConnection = new HashSet<PlaceEventConnection>();
+            Wishlist = new HashSet<Wishlist>();
+        }
+
+        public int EventId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<PlaceEventConnection> PlaceEventConnection { get; set; }
+        public ICollection<Wishlist> Wishlist { get; set; }
+    }
+}
---
+++ WhatToDo/Models/Wishlist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WhatToDo.Models
+{
+    public partial class Wishlist
+    {
+        public int WishlistId { get; set; }
+        public int UserId { get; set; }
+        public int EventId { get; set; }
+
+        public Event Event { get; set; }
+        public User User { get; set; }
+    }
+}
