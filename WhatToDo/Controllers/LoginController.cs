974e6f435d3062431d7146b1cc73eb7be21051a7
Всеволод Паршин <seva.parshin@yandex.ru>
2019-06-02 15:02:20 +0300

    feat: add sign in

--- WhatToDo/Controllers/LoginController.cs
+++ WhatToDo/Controllers/LoginController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using BCrypt.Net;
@@ -26,7 +28,7 @@ namespace WhatToDo.Controllers
         /// </summary>
         /// <remarks>
         /// Sample request:
-        /// POST /Login
+        /// /signup
         /// {
         ///   "Login": "johndoe",
         ///   "Password": "johndoe123"
@@ -37,6 +39,7 @@ namespace WhatToDo.Controllers
         /// <response code="201">Returns the newly created user</response>
         /// <response code="400">Invalid request data error</response>
         [HttpPost]
+        [Route("/signup")]
         [ProducesResponseType(201)]
         [ProducesResponseType(400)]
         public async Task<IActionResult> PostUser([FromBody] UserLogin user)
@@ -55,7 +58,51 @@ namespace WhatToDo.Controllers
             fullUser.JWT = Token(fullUser.Login);
             fullUser.Password = String.Empty;
 
-            return StatusCode(201, fullUser);
+            return StatusCode((int)HttpStatusCode.Created, fullUser);
+        }
+
+        /// <summary>
+        /// Authorizes user
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// /signin
+        /// {
+        ///   "Login": "johndoe",
+        ///   "Password": "johndoe123"
+        /// }
+        /// </remarks>
+        /// <param name="user">User's data to authorize</param>
+        /// <returns>Authorized user's data</returns>
+        /// <response code="200">Returns authorized user's data</response>
+        /// <response code="400">Invalid request data error</response>
+        /// <response code="401">Unsuccessful authorization</response>
+        [HttpPost]
+        [Route("/signin")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public IActionResult GetUser([FromBody] UserLogin user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            
+            var similarUser = _context.User.FirstOrDefault(u => u.Login == user.Login);
+            if (similarUser == null)
+                return StatusCode((int)HttpStatusCode.Unauthorized, "No users with same login found");
+            if (BCrypt.Net.BCrypt.Verify(user.Password, similarUser.Password))
+            {
+                similarUser.Password = String.Empty;
+                similarUser.JWT = Token(similarUser.Login);
+                return Ok(similarUser);
+            }
+            else
+            {
+                return StatusCode((int)HttpStatusCode.Unauthorized, "Wrong password");
+            }
         }
 
         private string Token(string login)
